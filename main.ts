634ed59ba5474132c71a5b654a494901e2b85a9f
ArtfulMonkey477 <76633179+ArtfulMonkey477@users.noreply.github.com>
2020-12-28 17:53:15 +0000

    v1 working

--- main.ts
+++ main.ts
@@ -1,16 +1,24 @@
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-radio.setGroup(99)
+radio.setGroup(8)
 radio.setTransmitPower(7)
 led.enable(false)
+basic.showArrow(ArrowNames.East)
 basic.forever(function () {
-    radio.sendValue("F", pins.digitalReadPin(DigitalPin.P10))
-    radio.sendValue("B", pins.digitalReadPin(DigitalPin.P8))
-    radio.sendValue("L", pins.digitalReadPin(DigitalPin.P11))
-    radio.sendValue("R", pins.digitalReadPin(DigitalPin.P9))
+    if (pins.digitalReadPin(DigitalPin.P8) == 0) {
+        radio.sendString("B")
+    } else {
+        if (pins.digitalReadPin(DigitalPin.P9) == 0) {
+            radio.sendString("L")
+        } else {
+            if (pins.digitalReadPin(DigitalPin.P10) == 0) {
+                radio.sendString("F")
+            } else {
+                if (pins.digitalReadPin(DigitalPin.P11) == 0) {
+                    radio.sendString("R")
+                } else {
+                    basic.clearScreen()
+                }
+            }
+        }
+    }
+    basic.pause(200)
 })
